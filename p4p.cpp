2c3f69b875a9fc5bca955a3f286eb0be9c63fe14
Mikael <mikael.p.persson@gmail.com>
2020-12-14 15:35:19 +0100

    cleaned up the cmake file, main is now an option to build, which defaults to on if its a standalone build, fixed source permissions

--- p4p.cpp
+++ p4p.cpp
@@ -27,19 +27,6 @@ PoseD p4p(const std::vector<cvl::Vector3D>& xs,
             xs[ indexes[0]],xs[ indexes[1]],xs[ indexes[2]],
             Rs,Ts);
 
-    // check that all of them look ok...
-    /*
-    for(int v=0;v<valid;++v)
-    {
-        PoseD P(Rs[v], Ts[v]);
-        for(int i=0;i<4;++i){
-            double err=((P*xs[indexes[i]]).dehom() - yns[indexes[i]]).squaredNorm();
-            std::cout<<"v: "<<v<<" i: "<<i<<" err: "<<err<<std::endl;
-        }
-    }
-    */
-
-
     // pick the minimum, whatever it is
     Vector2d y=yns[indexes[3]];
     Vector3d x=xs[indexes[3]];
@@ -57,27 +44,17 @@ PoseD p4p(const std::vector<cvl::Vector3D>& xs,
         Vector4d q=getRotationQuaternion(Rs[v]);
         q.normalize();
         PoseD tmp(q,Ts[v]);
+        if(!tmp.isnormal()) continue;
 
         Vector3d xr=tmp*x;
         if(xr[2]<0) continue;
-        double e=((Rs[v]*x + Ts[v]).dehom() - y).absSum();
+        double e=(xr.dehom()-y).squaredNorm();
         if (std::isnan(e)) continue;
         if (e<e0 ){
             P=tmp;
-            assert(P.isnormal());
             e0=e;
         }
     }
-    assert(P.isnormal());
-    // check that all of them look ok...
-
-
-    double error=0;
-    for(int i=0;i<4;++i){
-        double err=((P*xs[indexes[i]]).dehom() - yns[indexes[i]]).squaredNorm();
-        error+=err;
-    }
-    assert(!std::isnan(error));
 
     return P;
 }
